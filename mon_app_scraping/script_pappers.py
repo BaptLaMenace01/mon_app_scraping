b47cd41c22b5763dff0fac2778f49cafc2dc9c05
BaptLaMenace01 <baptquaino@hotmail.fr>
2025-07-24 13:47:51 +0200

    Update script_pappers.py

--- mon_app_scraping/script_pappers.py
+++ mon_app_scraping/script_pappers.py
@@ -8,15 +8,6 @@ from tqdm import tqdm
 API_KEY = "3d96bd10804182e3f44fa76e98527ee4e38f7539833546a0"
 
 def lancer_collecte_pappers(dossier_sortie, codes_postaux, max_age_annees=1, max_total=10):
-    """
-    Paramètres :
-    - dossier_sortie : chemin du dossier où enregistrer le fichier
-    - codes_postaux : liste de codes postaux à interroger
-    - max_age_annees : âge max des entreprises (ex : 1 = entreprises de moins d'un an)
-    - max_total : nombre maximal d'entreprises à collecter
-    """
-
-    # Date de création minimale = aujourd'hui - max_age_annees
     date_limite = datetime.today() - timedelta(days=365 * max_age_annees)
     fichier_csv = os.path.join(dossier_sortie, "fichier_conquete.csv")
     entreprises = []
